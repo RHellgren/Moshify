e0a721f57fc129bec6f83a64bdb797da8a94a31a
rhellgren <rhellgren@icloud.com>
2020-07-18 11:11:17 +0100

    Start XCode project

---
+++ Moshi_case/Moshi_case/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Moshi_case
+//
+//  Created by Robin Hellgren on 18/07/2020.
+//  Copyright © 2020 Robin Hellgren. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
